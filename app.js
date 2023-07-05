481c13713d18c21472a3800c09d67847a17c039f
havyas <havyasagaltha@gmail.com>
2023-07-05 09:32:54 +0530

    added some information to html page

--- app.js
+++ app.js
@@ -15,10 +15,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+  res.render("home",{homestart:homeStartingContent})
+})
 
+app.get("/about",function(req,res){
+  res.render("about",{about:aboutContent})
+})
 
-
-
+app.get("/contact",function(req,res){
+  res.render("contact",{contact:contactContent})
+})
 
 
 
